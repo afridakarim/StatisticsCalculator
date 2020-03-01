6ab93f4693b5b85b5933c553731edb8b1af2e0e9
afridakarim <ak2577@njit.edu>
2020-02-29 22:10:19 -0500

    random select

--- Statistics/randomData.py
+++ Statistics/randomData.py
@@ -1,6 +1,7 @@
 import random
 import pprint
-import decimal
+
+
 
 def random_code():
     random.seed(6)
@@ -45,3 +46,37 @@ def random_code_no_seed():
     pprint.pprint(rand_one)
     pprint.pprint(rand_two)
     pprint.pprint(num_list)
+
+def random_select():
+    random.seed(6)
+    randomData1 = []
+    randomData2 = []
+
+    i = 0
+    rand_list = [10,20,30,40,50,60]
+    rand_valueList = random.choices(rand_list,k=4)
+    randomData1.append(rand_valueList)
+    i+=1
+
+    rand_value = random.choice(rand_list)
+    randomData2.append(rand_value)
+
+    pprint.pprint(randomData1)
+    pprint.pprint(randomData2)
+
+def random_select_no_seed():
+    random.seed()
+    randomData1 = []
+    randomData2 = []
+
+    i = 0
+    rand_list = [10, 20, 30, 40, 50, 60]
+    rand_valueList = random.choices(rand_list, k=4)
+    randomData1.append(rand_valueList)
+    i += 1
+
+    rand_value = random.choice(rand_list)
+    randomData2.append(rand_value)
+
+    pprint.pprint(randomData1)
+    pprint.pprint(randomData2)
