53b56cd2523c0a8797fe449b72eb32bbcfa4d14d
afridakarim <ak2577@njit.edu>
2020-02-29 23:53:10 -0500

    mean

--- Statistics/randomData.py
+++ Statistics/randomData.py
@@ -21,9 +21,9 @@ def random_code():
     rand_one = random.randint(1,50)
     rand_two = round(value,2)
 
-    pprint.pprint(rand_one)
-    pprint.pprint(rand_two)
-    pprint.pprint(num_list)
+    #pprint.pprint(rand_one)
+    #pprint.pprint(rand_two)
+    #pprint.pprint(num_list)
 
 def random_code_no_seed():
     random.seed()
@@ -43,40 +43,36 @@ def random_code_no_seed():
     rand_one = random.randint(1,50)
     rand_two = round(value,2)
 
-    pprint.pprint(rand_one)
-    pprint.pprint(rand_two)
-    pprint.pprint(num_list)
+    #pprint.pprint(rand_one)
+    #pprint.pprint(rand_two)
+    #pprint.pprint(num_list)
 
 def random_select():
     random.seed(6)
     randomData1 = []
-    randomData2 = []
 
     i = 0
     rand_list = [10,20,30,40,50,60]
     rand_valueList = random.choices(rand_list,k=4)
-    randomData1.append(rand_valueList)
     i+=1
 
     rand_value = random.choice(rand_list)
-    randomData2.append(rand_value)
+    randomData1.append(rand_value)
 
-    pprint.pprint(randomData1)
-    pprint.pprint(randomData2)
+    #pprint.pprint(randomData1)
+    #pprint.pprint(rand_valueList)
 
 def random_select_no_seed():
     random.seed()
     randomData1 = []
-    randomData2 = []
 
     i = 0
     rand_list = [10, 20, 30, 40, 50, 60]
     rand_valueList = random.choices(rand_list, k=4)
-    randomData1.append(rand_valueList)
     i += 1
 
     rand_value = random.choice(rand_list)
-    randomData2.append(rand_value)
+    randomData1.append(rand_value)
 
-    pprint.pprint(randomData1)
-    pprint.pprint(randomData2)
+    #pprint.pprint(randomData1)
+    #pprint.pprint(rand_valueList)
